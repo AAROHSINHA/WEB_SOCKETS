864be0d912903a509d79cff4932162cb7c846782
Aaroh Sinha <aarohsinha.programming@gmail.com>
2026-06-16 20:03:45 +0530

    feat: basic websocket echo connection

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# 1. Define the exact addresses that are allowed to talk to your server
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
-# Enable CORS so your React frontend can access the API
+# 2. Add the CORS middleware to the application
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"], # Vite and CRA ports
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=origins,  # Only allow these specific addresses
+    allow_credentials=True, # Allows cookies and authentication headers
+    allow_methods=["*"],    # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],    # Allows all headers
 )
+@app.websocket("/ws/echo")
+async def websocket_endpoint(websocket: WebSocket):
+    # Accept an incoming web socket request
+    await websocket.accept()
+    # This is the basic start point. When an http request comes demanding
+    # for a websocket connection, we accept that
+    print("==> NEW WEBSOCKET CONNECTION ESTABLISHED")
 
-@app.get("/")
-def hello():
-    return {"success": True, "message": "Welcome to fastapi"}
+    try:
+        while True:
+            # This is infinite loop. signifies constant connection
+            # when data comes we recieve it and here in this case we resent it back
+            data = await websocket.receive_text()
+            print("==> RECIEVED TEXT - ", data)
+            await websocket.send_text(f"==> BOUNCED BACK SAME DATA FROM SERVER - {data}")
+
+    # This when the websocket disconencts. network turns off etc
+    except WebSocketDisconnect:
+        print("==> CONNECTION FROM CLIENT DROPPED")
+
+
